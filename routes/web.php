bb674e5ca90ee0dca5ea2716d4382607f6859912
felipefuks <felipemateus112012@gmail.com>
2025-06-04 14:44:19 -0300

    adjust default route

--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 })->middleware('guest');
 
 Route::get('/dashboard', function () {
